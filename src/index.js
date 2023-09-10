e34f4a703ec9638bd2fa0fcc3854e67de2645fbf
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-10 17:38:02 +0530

    implement  method

--- src/index.js
+++ src/index.js
@@ -26,3 +26,45 @@ function pathParser(separator, optionalChar) {
     }),
   });
 }
+
+function traversePath(obj, props) {
+  debugger;
+  let i = 0;
+  let at = obj;
+  if (!at[props[0].name]) return undefined;
+  while (i < props.length - 1) {
+    const prop = props[i];
+    if (!at[prop.name]) return undefined;
+    at = at[prop.name];
+    ++i;
+  }
+  return at;
+}
+
+exports.removeOwnProp = function (
+  obj,
+  path,
+  parserOptions = {
+    separator: DEFAULT_SEPARATOR,
+    optionalChar: DEFAULT_OPTIONAL_CHAR,
+  }
+) {
+  if (!(obj instanceof Object)) throw errors.ObjectInvalid();
+  const parsedPath = pathParser(
+    parserOptions.separator,
+    parserOptions.optionalChar
+  )(path);
+  const props = parsedPath.props; // all props in a given path
+
+  debugger;
+  const targetProp = props[props.length - 1];
+  const targetObject = traversePath(obj, props);
+
+  if (!targetObject && !targetProp.isOptional)
+    throw errors.PropMissing(
+      `Prop named "${targetProp.name}" missing on the given path "${path}"`
+    );
+
+  if (targetObject) return delete targetObject[targetProp.name];
+  return false;
+};
